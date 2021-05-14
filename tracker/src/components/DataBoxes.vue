60729b1cea5d24d7742341db988c8d8032b14b7d
Sarath Kaul <kaul.sarath@gmail.com>
2021-05-14 19:44:53 +0530

    Recovered Box Added

--- tracker/src/components/DataBoxes.vue
+++ tracker/src/components/DataBoxes.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="grid md:grid-cols-2 gap-4">
+    <div class="grid md:grid-cols-3 gap-4">
         <div class="shadow-md bg-blue-100 p-10 text-center rounded">
             <h3 class="text-3xl text-blue-900 font-bold mb-4">
                 Cases
@@ -31,6 +31,25 @@
             <div class="text-2xl mb-4">
                 <span class="font-bold">Total:</span>
                 {{numberWithCommas(stats.TotalDeaths)}}
+                
+            </div>
+        </div>
+
+
+
+        <div class="shadow-md bg-green-200 p-10 text-center rounded">
+            <h3 class="text-3xl text-blue-900 font-bold mb-4">
+                Recovered
+            </h3>
+
+            <div class="text-2xl mb-4">
+                <span class="font-bold">New:</span>
+                {{numberWithCommas(stats.NewRecovered)}}
+                
+            </div>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">Total:</span>
+                {{numberWithCommas(stats.TotalRecovered)}}
                 
             </div>
         </div>
