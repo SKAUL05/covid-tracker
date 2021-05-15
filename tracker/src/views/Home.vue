bbadb84e9d2590c7dffcd5c75b9610e25a676766
Sarath Kaul <kaul.sarath@gmail.com>
2021-05-15 23:20:17 +0530

    News Tab Added

--- tracker/src/views/Home.vue
+++ tracker/src/views/Home.vue
@@ -1,19 +1,47 @@
 <template>
-  <main v-if="!loading">
-    <DataTitle :text="title" :dataDate="dataDate"/>
-    <DataBoxes :stats="stats" />
-    <CountrySelect @get-country="getCountryData" :countries="countries"/>
-    <button @click="clearCountryData" v-if="stats.Country" class="bg-green-700 text-white roounded p-3 mt-8 focus:outline-none hover:bg-green-600 mb-2" >
-      Clear Country
-    </button>
-  </main>
-  <main class="flex flex-col align-center justify-center text-center" v-else>
-    <div class="text-gray-500 text-3xl mt-10 mb-6">
-      Fetching Data
-    </div>
-  <img :src="loadingImage" class="w-24 m-auto" alt="" />
+    <div class="flex flex-wrap">
+    <div class="w-full">
+      <ul class="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row">
+        <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
+          <a class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal" v-on:click="toggleTabs(1)" v-bind:class="{'text-black-600 bg-white': openTab !== 1, 'text-white bg-blue-600': openTab === 1}">
+            Stats
+          </a>
+        </li>
+        <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
+          <a class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal" v-on:click="toggleTabs(2)" v-bind:class="{'text-black-600 bg-white': openTab !== 2, 'text-white bg-blue-600': openTab === 2}">
+            News
+          </a>
+        </li>
+      </ul>
+      <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
+        <div class="px-4 py-5 flex-auto">
+          <div class="tab-content tab-space">
+            <div v-bind:class="{'hidden': openTab !== 1, 'block': openTab === 1}">
+              <main v-if="!loading">
+                  <DataTitle :text="title" :dataDate="dataDate"/>
+                  <DataBoxes :stats="stats" />
+                  <CountrySelect @get-country="getCountryData" :countries="countries"/>
+                  <button @click="clearCountryData" v-if="stats.Country" class="bg-green-700 text-white roounded p-3 mt-8 focus:outline-none hover:bg-green-600 mb-2" >
+                    Clear Country
+                  </button>
+              </main>
+              <main class="flex flex-col align-center justify-center text-center" v-else>
+                <div class="text-gray-500 text-3xl mt-10 mb-6">
+                  Fetching Data
+                </div>
+                <img :src="loadingImage" class="w-24 m-auto" alt="" />
 
-  </main>
+              </main>
+            </div>
+            <div v-bind:class="{'hidden': openTab !== 2, 'block': openTab === 2}">
+                <News :articles="articles" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
 </template>
 
 <script>
@@ -21,13 +49,15 @@
 import DataTitle from '@/components/DataTitle'
 import DataBoxes from '@/components/DataBoxes'
 import CountrySelect from '@/components/CountrySelect'
+import News from '@/components/News'
 
 export default {
   name: 'Home',
   components: {
     DataTitle,
     DataBoxes,
-    CountrySelect
+    CountrySelect,
+    News
   },
   data() {
     return {
@@ -36,12 +66,19 @@ export default {
       dataDate: '',
       stats: {},
       countries: [],
-      loadingImage: require('../assets/hourglass.gif')
+      articles: [],
+      loadingImage: require('../assets/hourglass.gif'),
+      openTab: 1
     }
   },
   methods: {
     async fetchCovidData() {
       const res = await fetch('https://api.covid19api.com/summary')
+      const data = await res.json()
+      return data
+    },
+    async fetchNews() {
+      const res = await fetch('https://newsapi.org/v2/everything?q=covid&sortBy=publishedAt&apiKey=4dbc17e007ab436fb66416009dfb59a8&language=en')
       const data = await res.json()
       return data
     },
@@ -55,16 +92,23 @@ export default {
       this.title = "Global"
       this.stats = data.Global
       this.loading = false
-}
+},
+ toggleTabs: function(tabNumber){
+      this.openTab = tabNumber
+    }
   },
   async created(){
     console.log("Created.....")
     const data = await this.fetchCovidData()
     console.log(data)
+    const newsData = await this.fetchNews()
+    console.log(newsData)
+    this.articles = newsData.articles
     this.dataDate = data.Date
     this.stats = data.Global
     this.countries = data.Countries
     this.loading = false
+
   }
 }
 </script>
